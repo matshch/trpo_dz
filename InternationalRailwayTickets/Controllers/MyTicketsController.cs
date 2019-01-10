cc1aaa5191bf3744680e766d5f2046739b5e4325
Artem Leshchev <matshch@gmail.com>
2019-01-10 05:44:54 +0300

    MyTicketsController was misplaced ALL THIS TIME

--- InternationalRailwayTickets/Controllers/MyTicketsController.cs
+++ InternationalRailwayTickets/Controllers/MyTicketsController.cs
@@ -7,7 +7,7 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace InternationalRailwayTickets.Views
+namespace InternationalRailwayTickets.Controllers
 {
     [Authorize]
     public class MyTicketsController : Controller
